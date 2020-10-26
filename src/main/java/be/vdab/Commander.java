6220b5d7ff6bab8c228241ccac0ee223f5f7a434
Thomas Veris <thomas.veris.me@gmail.com>
2020-10-26 01:57:45 +0100

    Started implementation of commander and various commands

---
+++ src/main/java/be/vdab/Commander.java
@@ -0,0 +1,27 @@
+package be.vdab;
+
+import be.vdab.commands.Command;
+
+public class Commander {
+    public enum Commands {;
+        //TODO: choose commands and implement them
+        //GET_KDA("Get KDA of this player", new Command()),
+
+
+        private String commandDescription;
+        private Command command;
+
+        Commands(String commandDescription, Command command) {
+            this.commandDescription = commandDescription;
+            this.command = command;
+        }
+
+        public String getCommandDescription() {
+            return commandDescription;
+        }
+
+        public Command getCommand() {
+            return command;
+        }
+    }
+}
---
+++ src/main/java/be/vdab/commands/Command.java
@@ -0,0 +1,5 @@
+package be.vdab.commands;
+
+public interface Command {
+    void execute();
+}
